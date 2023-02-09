7f08b9d399372e1acc307497561376869e8ae674
CFraissinet <clement-fraissinet@orange.fr>
2023-02-10 00:47:14 +0100

    Ajout fonction randomJoueur

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const url ='https://api-esportplayers.vercel.app/teamsByLeague/LEC'
+const url ='https://api-esportplayers.vercel.app/randomTeamByLeague/LEC'
 fetch(url)
 .then(reponse=>reponse.json())
 .then(data=>{
     console.log(data);
-    console.log(findPlayer(data,"Caps"))
+    console.log(randomPlayer(data));
 })
 .catch(error => {
     console.log(error);
@@ -17,7 +17,23 @@ function findPlayer(data,name){
   }
 }
 
-/* fetch random team 
+function randomPlayer(data){
+
+  let numPlayer = Math.floor(Math.random() * (data.team.players.length - 0 + 1)) + 0;
+  return data.team.players[numPlayer].summonerName;
+ 
+}
+
+function verifPlayer (data,reponse,name){
+  if(document.getElementById('joueur')===randomPlayer(data)){
+    return "Bonne reponse"
+  }else{
+    return "Mauvaise Reponse"
+  }
+}
+
+/* 
+fetch random team 
 prendre la taille data.players
 generer un chiffre aléatoire entre 0 et la taille tableau au dessus
 faire un console log data.player[chiffre aléatoire]
